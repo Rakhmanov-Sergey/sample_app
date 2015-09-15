27b9f91687375410ef68edacb8cdb24dad480d93
Sergey Rakhmanov <rakhmanov.sergey@gmail.com>
2015-09-15 19:46:44 +0300

    Finish with static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,6 +7,11 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title('ROR Tutorial | Home')
+    end
   end
 
   describe "Help page" do
@@ -14,12 +19,22 @@ describe "Static pages" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title('ROR Tutorial | Help')
+    end
   end
 
   describe "About page" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title('ROR Tutorial | About')
     end
   end
 end

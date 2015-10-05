1fc3160330c83c3eb97d27bf3ed3e645db3c394f
Sergey Rakhmanov <rakhmanov.sergey@gmail.com>
2015-10-05 20:34:16 +0300

    Modify user page

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -10,4 +10,14 @@ describe "User pages - " do
     it "Should have h1 'Sign up'"    do should have_selector('h1', text: 'Sign up') end
     it "Should have title 'Sign up'" do should have_title(full_title('Sign up')) end
   end
+
+  describe "Profile page -" do
+    before { visit user_path(user) }
+
+    let(:user) { FactoryGirl.create(:user) }
+    let(:name) {user.name}
+
+    it "Should have user name in content " do should have_content(name) end
+    it "Should have user name in title "   do should have_title(name) end
+  end
 end

056577ebbf581fc2af2c323dada2923ea4489096
Sergey Rakhmanov <rakhmanov.sergey@gmail.com>
2015-10-06 11:17:24 +0300

    Invalid registration

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,11 +1,32 @@
 require 'spec_helper'
 
-describe "User pages - " do
+describe "User pages -" do
 
   subject { page }
 
-  describe "SignUp page - " do
+  describe "SignUp page -" do
     before { visit signup_path }
+
+    let(:submit) { "Create my account" }
+
+    describe "With invalid information -" do
+      it "Should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "With valid information -" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      it "Should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
 
     it "Should have h1 'Sign up'"    do should have_selector('h1', text: 'Sign up') end
     it "Should have title 'Sign up'" do should have_title(full_title('Sign up')) end

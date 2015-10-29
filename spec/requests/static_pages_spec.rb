46e91d1dd8000219484ed20102d42b9ade9f5ee9
Sergey Rakhmanov <rakhmanov.sergey@gmail.com>
2015-10-29 21:37:09 +0300

    Add followings and followers form to users show and home pages. Add follow and unfollow buttons

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -33,18 +33,33 @@ describe "Static pages -" do
     it_should_behave_like "all static pages"
     it "Should not have the title '| Home'" do should_not have_title('| Home') end
 
-    describe "For signed-in users" do
+    describe "for signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
       before do
-        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
-        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+        FactoryGirl.create(:micropost, user: user, content: "Lorem")
+        FactoryGirl.create(:micropost, user: user, content: "Ipsum")
         sign_in user
         visit root_path
       end
 
-      it "Should render the user's feed" do
+      it "should render the user's feed" do
         user.feed.each do |item|
           expect(page).to have_selector("li##{item.id}", text: item.content)
+        end
+      end
+
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it "Should have 0 following users" do
+          should have_link("0 following", href: following_user_path(user))
+        end
+        it "Should have 1 follower" do
+          should have_link("1 followers", href: followers_user_path(user))
         end
       end
     end
